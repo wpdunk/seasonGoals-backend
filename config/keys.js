afeee71fc526c5aa58041e788271e1dbc6cbdaa7
William Dunk <williamdunk@gmail.com>
2019-03-11 10:09:54 +0000

    :sparkles: added User login/register functionality

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://p-lefevere:Q7hjAAjL2mZwWeg@g-o-a-l-s-fjqwx.mongodb.net/test?retryWrites=true",
+  secretOrKey: "secret"
+};
--- server.js
+++ server.js
@@ -4,21 +4,50 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const targetRoutes = express.Router();
-const PORT = 4000;
+// const PORT = 4000;
+
+const passport = require("passport");
+const users = require("./routes/api/users");
 
 let Target = require("./target.model");
 
 app.use(cors());
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 app.use(bodyParser.json());
 
-mongoose.connect(
-  "mongodb://127.0.0.1:27017/targets",
-  { useNewUrlParser: true }
-);
-const connection = mongoose.connection;
-connection.once("open", function() {
-  console.log("MongoDB database connection established successfully");
-});
+// DB Config
+const db = require("./config/keys").mongoURI;
+// Connect to MongoDB
+console.log(db);
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+// LOCAL DB CONFIG
+// mongoose.connect(
+//   "mongodb://127.0.0.1:27017/targets",
+//   { useNewUrlParser: true }
+// );
+// const connection = mongoose.connection;
+// connection.once("open", function() {
+//   console.log("MongoDB database connection established successfully");
+// });
+
+// Passport Middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
 
 targetRoutes.route("/").get(function(req, res) {
   Target.find(function(err, targets) {
@@ -84,6 +113,11 @@ targetRoutes.route("/delete/:id").delete(function(req, res) {
 
 app.use("/targets", targetRoutes);
 
-app.listen(PORT, function() {
-  console.log("Server is running on Port: " + PORT);
-});
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+
+// app.listen(PORT, function() {
+//   console.log("Server is running on Port: " + PORT);
+// });
